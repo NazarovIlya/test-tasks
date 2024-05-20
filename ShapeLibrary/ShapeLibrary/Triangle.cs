306d5254329413d01e474aef7624f57433a1a4d1
NazarovIlya <eliakohai@gmail.com>
2024-05-20 16:55:42 +0300

    Triangle class added

---
+++ ShapeLibrary/ShapeLibrary/Triangle.cs
@@ -0,0 +1,15 @@
+﻿namespace ShapeLibrary
+{
+  public class Triangle(double FirstSide, double SecondSide, double ThirdSide) : Shape
+  {
+    public override double GetArea()
+    {
+      //Расчет площади треугольника по трем сторонам (формула Герона)
+      double semiPerimeter = (FirstSide + SecondSide + ThirdSide) / 2;
+
+      return Math.Sqrt(semiPerimeter * (semiPerimeter - FirstSide)
+                                     * (semiPerimeter - SecondSide)
+                                     * (semiPerimeter - ThirdSide));
+    }
+  }
+}
