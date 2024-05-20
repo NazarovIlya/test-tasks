e7f56e8c5ce14b27ee22fda51ca2208d7d68c906
NazarovIlya <eliakohai@gmail.com>
2024-05-20 16:57:54 +0300

    updated

--- ShapeLibrary/Shapes/Program.cs
+++ ShapeLibrary/Shapes/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ShapeLibrary;
+
+double radius = 5,
+       fisrtSide = 3,
+       secondSide = 4,
+       thirdSide = 5;
+
+Circl circl = new(radius);
+Triangle triangle = new(fisrtSide, secondSide, thirdSide);
+
+
+double squareOfCircl = circl.GetArea();
+double squareOfTriangle = circl.GetArea();
+
+
+Console.WriteLine(circl.ToString());
+Console.WriteLine(triangle.ToString());
