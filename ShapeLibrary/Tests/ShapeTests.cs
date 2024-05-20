db9649f10268b48b15ca498bafd85026e7a0746d
NazarovIlya <eliakohai@gmail.com>
2024-05-21 01:15:32 +0300

    tests updated

--- ShapeLibrary/Tests/ShapeTests.cs
+++ ShapeLibrary/Tests/ShapeTests.cs
@@ -6,10 +6,30 @@ namespace Tests
   public class ShapeTests
   {
     [TestMethod]
-    public void IsTriangle()
+    public void IsRightCalculate()
     {
       Triangle triangle = new(3, 3, 3);
-      Assert.AreEqual(3.897114317029974, triangle.GetArea());
+      Assert.AreEqual(3.8971, Math.Round(triangle.GetArea(), 4));
+
+      triangle = new Triangle(5, 5, 5);
+      Assert.AreEqual(10.8253, triangle.GetArea(), 4);
+
+      triangle = new Triangle(5, 8, 10);
+      Assert.AreEqual(19.81, triangle.GetArea(), 2);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void IsArgumentExeption()
+    {
+      Triangle triangle = new(2, 2, 5);
+      triangle.GetArea();
+
+      triangle = new Triangle(5, 5, 11);
+      triangle.GetArea();
+
+      triangle = new Triangle(10, 10, 21);
+      triangle.GetArea();
     }
   }
 }
