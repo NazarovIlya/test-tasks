233e16aff893daed96e58e94db602e9a82365225
NazarovIlya <eliakohai@gmail.com>
2024-05-21 11:36:12 +0300

    new exception added

---
+++ ShapeLibrary/ShapeLibrary/Exceptions/ArgumentNotPositiveValueException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary.Exceptions
+{
+  internal class ArgumentNotPositiveValueException(string? message) : Exception(message)
+  {
+  }
+}
